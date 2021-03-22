90dff5c362306f7b2bdbdd7fec0b8abf98884bbf
Andrew Braun <andrew@andrewbraun.dev>
2021-03-22 23:50:05 +0400

    feat: Intial file-based routing design

---
+++ pages/events/[event-id]/index.js
@@ -0,0 +1,11 @@
+import styles from "./single-event.module.css"
+
+function SingleEvent() {
+	return (
+		<div>
+			<h1>Single Event</h1>
+		</div>
+	)
+}
+
+export default SingleEvent
---
+++ pages/events/index.js
@@ -0,0 +1,11 @@
+import styles from "./events.module.css"
+
+function EventsPage() {
+	return (
+		<div>
+			<h1>All Events</h1>
+		</div>
+	)
+}
+
+export default EventsPage
