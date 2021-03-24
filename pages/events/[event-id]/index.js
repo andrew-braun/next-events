018eec91d94d9deef1f9641c62d3f3adedebc0c3
Andrew Braun <andrew@andrewbraun.dev>
2021-03-24 18:30:54 +0400

    feat: Add prebuilt event components

--- pages/events/[event-id]/index.js
+++ pages/events/[event-id]/index.js
@@ -1,11 +1,39 @@
+import { Fragment } from "react"
+import { useRouter } from "next/router"
+import { getEventById } from "../../../dummy-data"
+import EventSummary from "../../../components/events/event-detail/EventSummary/event-summary"
+import EventLogistics from "../../../components/events/event-detail/EventLogistics/event-logistics"
+import EventContent from "../../../components/events/event-detail/EventContent/event-content"
 import styles from "./single-event.module.css"
 
-function SingleEvent() {
+function SingleEventPage() {
+	const router = useRouter()
+
+	const eventId = router.query["event-id"]
+
+	const event = getEventById(eventId)
+
+	if (!event) {
+		return <p>Event not found</p>
+	}
+
+	const { title, description, location, date, image } = event
+
+	console.log(location)
 	return (
-		<div>
-			<h1>Single Event</h1>
-		</div>
+		<Fragment>
+			<EventSummary title={title} />
+			<EventLogistics
+				address={location}
+				date={date}
+				image={image}
+				imageAlt={title}
+			/>
+			<EventContent>
+				<p>{description}</p>
+			</EventContent>
+		</Fragment>
 	)
 }
 
-export default SingleEvent
+export default SingleEventPage
