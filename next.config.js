05d2698e245efe048efa0ece5e6aac1f7568f7cd
Andrew Braun <andrew@andrewbraun.dev>
2021-03-27 17:19:06 +0400

    build: Asset prefixes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const ghPages = process.env.DEPLOY_TARGET === "gh-pages"
 
 module.exports = {
-	assetPrefix: ghPages ? "/andrew-braun.github.io/next-events/" : "", // customize this value
+	assetPrefix: ghPages ? "/andrew-braun.github.io/next-events" : "", // customize this value
 }
